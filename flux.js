812754653c67e2fb0c51c7cb5ab745699e8c835d
Gautam Suthar <iamgautamsuthar@gmail.com>
2025-02-11 19:31:40 +0530

    add package information fetching functionality in flux.js

--- flux.js
+++ flux.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+
+import axios from 'axios';
+import tar from 'tar';
+import yargs from 'yargs';
+import fs from 'fs';
+import path from 'path';
+
+const getPackageInformation = async (packageName) => {
+    try {
+        const url = `https://registry.npmjs.org/${pkgName}`;
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.log(`Error while fetching package information: ${error}`);
+        process.exit(1);
+    }
+};
