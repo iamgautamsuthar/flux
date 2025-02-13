03ae144f9f73139e91db0e00b846359e8ab2b088
Gautam Suthar <iamgautamsuthar@gmail.com>
2025-02-13 23:18:36 +0530

    Enhance PowerShell installation script: add admin check, improve download and extraction messages, and run npm install post-extraction.

--- install/install_flux.ps1
+++ install/install_flux.ps1
@@ -4,7 +4,6 @@ $extractPath = "$env:TEMP\flux"
 $fluxInstallPath = "C:\Program Files\nodejs\node_modules\flux"
 $nodejsPath = "C:\Program Files\nodejs"
 
-# Administrator
 $adminCheck = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
 if (-not $adminCheck.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Host "Please run this script as Administrator." -ForegroundColor Red
@@ -13,18 +12,14 @@ if (-not $adminCheck.IsInRole([Security.Principal.WindowsBuiltInRole]::Administr
     exit 1
 }
 
-# Download the GitHub repository
 Write-Host "Downloading flux repository..."
 Invoke-WebRequest -Uri $githubRepoUrl -OutFile $tempZip
 
-# Extract the ZIP file
 Write-Host "Extracting flux repository..."
 Expand-Archive -Path $tempZip -DestinationPath $extractPath -Force
 
-# Find the extracted folder 
 $extractedFolder = Get-ChildItem -Path $extractPath | Where-Object { $_.PSIsContainer } | Select-Object -First 1
 
-# Check if the folder exists and rename it to "flux"
 if ($null -ne $extractedFolder) {
     Write-Host "Renaming folder to 'flux'..."
     Rename-Item -Path $extractedFolder.FullName -NewName "flux"
@@ -33,21 +28,17 @@ if ($null -ne $extractedFolder) {
     exit 1
 }
 
-# Move the "flux" folder to the node_modules directory
 Write-Host "Moving flux to $fluxInstallPath..."
 if (Test-Path $fluxInstallPath) { Remove-Item -Recurse -Force $fluxInstallPath }
 Move-Item -Path "$extractPath\flux" -Destination $fluxInstallPath
 
-# Run `npm install` inside the flux folder
 Set-Location -Path $fluxInstallPath
 Start-Process -NoNewWindow -FilePath "npm" -ArgumentList "install" -Wait
 
 
-# Copy install scripts to C:\Program Files\nodejs
 Write-Host "Copying install scripts to $nodejsPath..."
 $installPath = "$fluxInstallPath\install"
 
-# Check if the install folder exists before copying
 if (Test-Path $installPath) {
     Write-Host "Found install folder. Copying scripts..."
     Copy-Item -Path "$installPath\flux" -Destination "$nodejsPath\flux" -Force
